aa8d2363fd7e9df1aa08b4c66f48893fcbb2d84a
Dean Brettle <dean@brettle.com>
2015-08-12 00:45:09 -0700

    Use versions from 1.0 API to avoid incompatibilities.

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.0');
   api.use('brettle:accounts-anonymous@0.0.1');
   api.use('accounts-base');
   api.use('tracker');
