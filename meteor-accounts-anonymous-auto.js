932cd22edd41ed74e613534c9eb43f050b06d9b9
Dean Brettle <dean@brettle.com>
2015-08-17 22:07:57 -0700

    Publish 0.3.1 which removes spurious console message.

--- meteor-accounts-anonymous-auto.js
+++ meteor-accounts-anonymous-auto.js
@@ -9,7 +9,6 @@ if (Meteor.isClient) {
   Meteor.startup(function(){
     Tracker.autorun(function () {
       if (! Meteor.userId() && AccountsAnonymousAuto._enabled.get()) {
-        console.log('Logging in anonymously');
         AccountsAnonymous.login();
       }
     });
--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'brettle:accounts-anonymous-auto',
-  version: '0.3.0',
+  version: '0.3.1',
   // Brief, one-line summary of the package.
   summary: 'Automatically create anonymous accounts for each new visitor',
   // URL to the Git repository containing the source code for this package.
